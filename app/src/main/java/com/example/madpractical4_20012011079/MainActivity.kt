012b845373fbee91feb79223b6aef503498e5c5b
Bhavya Patel <110376349+bhavyapatel1551@users.noreply.github.com>
2022-09-12 18:13:45 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/madpractical4_20012011079/MainActivity.kt
+++ app/src/main/java/com/example/madpractical4_20012011079/MainActivity.kt
@@ -18,45 +18,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonBrowse.setOnClickListener {
-//            Toast.makeText(this, "Browse Button Clicked", Toast.LENGTH_SHORT).show()
             Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://uvpce.guni.ac.in")).apply {
                 startActivity(this)
             }
         }
 
         binding.buttonCall.setOnClickListener {
-//            Toast.makeText(this, "Call Button Clicked", Toast.LENGTH_SHORT).show()
             Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:6353744879")).apply {
                 startActivity(this)
             }
         }
         binding.buttonContact.setOnClickListener {
-//            Toast.makeText(this, "Contact Button Clicked", Toast.LENGTH_SHORT).show()
             Intent(Intent.ACTION_VIEW).setType(ContactsContract.Contacts.CONTENT_TYPE).apply {
                 startActivity(this)
             }
         }
         binding.buttonCallLog.setOnClickListener {
-//            Toast.makeText(this, "Call_Log Button Clicked", Toast.LENGTH_SHORT).show()
             Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).apply {
                 startActivity(this)
             }
 
         }
         binding.buttonGallery.setOnClickListener {
-//            Toast.makeText(this, "Gallery Button Clicked", Toast.LENGTH_SHORT).show()
             Intent(Intent.ACTION_VIEW).setType("image/*").apply {
                 startActivity(this)
             }
         }
         binding.buttonCamera.setOnClickListener {
-//            Toast.makeText(this, "Camera Button Clicked", Toast.LENGTH_SHORT).show()
             Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
                 startActivity(this)
             }
         }
         binding.buttonAlarm.setOnClickListener {
-//            Toast.makeText(this, "Alarm Button Clicked", Toast.LENGTH_SHORT).show()
             Intent(AlarmClock.ACTION_SHOW_ALARMS).apply {
                 startActivity(this)
             }
